b3c6fca005c708b7a31f462f3098ff92741eb1f5
hirwajeaneric <hirwajeaneric25@gmail.com>
2022-08-25 16:19:49 +0200

    On tutorial 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import FirstStep from './components/FirstStep';
 
 function App() {
   return (
     <div className="App">
-
+      <header className='App-header'>
+        <h3 style={{color: 'red', textDecoration: "underline"}}>ReactJS Multi Step Application</h3>
+        <FirstStep />
+      </header>
     </div>
   );
 }
---
+++ src/components/SecondStep.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Button, TextField } from '@mui/material';
+
+const SecondStep = () => {
+  return (
+    <div>
+        <div>
+            <TextField label='First name' margin='normal' variant='outlined' color='secondary' />
+        </div>
+        <div>
+            <TextField label='Last name' margin='normal' variant='outlined' color='secondary' />
+        </div>
+        <div>
+            <TextField label='Contact number' margin='normal' variant='outlined' color='secondary' />
+        </div>
+        <div>
+            <Button variant="contained" color="primary">Next</Button>
+        </div>
+    </div>
+  )
+}
+
+export default SecondStep
